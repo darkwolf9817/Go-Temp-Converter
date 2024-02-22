a76e53e0862f75f8e49ac9b97c2209ebe33f27f8
Tyler Kearney <75549435+darkwolf9817@users.noreply.github.com>
2024-02-21 20:47:51 -0600

    Restarting almost
    Trying something else with it

--- main.go
+++ main.go
@@ -1,36 +1,14 @@
 package Go_Temp_Converter
 
 import (
-	"fmt"
-	"strconv"
-),
+	"fmt",
 	"strconv",
 
 	"fyne.io/fyne/v2",
+	"fyne.io/fyne/v2/app",
 	"fyne.io/fyne/v2/container",
+	"fyne.io/fyne/v2/dialog",
+	"fyne.io/fyne/v2/theme",
 	"fyne.io/fyne/v2/widget"
-	)
+)
 
-func main() {
-
-}
-
-app := fyne.NewApp("Temperature Converter")
-window := app.NewWindow("Convert!")
-
-// Define UI Elements
-unitDropdown := widget.NewSelect([]string{"Celsius", "Fahrenheit", "Kelvin"}, func(s string) {
-	outLabel.SetText("")
-})
-
-inEntry := widget.NewEntry()
-
-convertBtn := widget.NewButton("Convert", func() {
-	value, err := strconv.ParseFloat(inEntry.Text(), 64)
-	if err != nil {
-		fmt.Println("Error parsing input:", err)
-		return
-	}
-	fromUnit := unitDropdown.Selected
-	toUnit := 
-})
